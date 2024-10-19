95a223ea6872e4633d4fab691fbeb57f79abaea1
Ridzwan <ridzwangunawan10@gmail.com>
2024-10-19 08:55:06 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Ridzwan Gunawan"; // TODO: replace this
+const studentId = "FS11631991"; // TODO: replace this
+
+export { url, studentName, studentId };
